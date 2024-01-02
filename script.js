7b8b22f31073e59462d6b7dc3a9a72172770e2d1
adamsJS23 <adamaguebre@yahoo.fr>
2024-01-02 19:16:32 +0100

    created the neccessary variables in script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const fromText = document.querySelector(".from-text"),
+  toText = document.querySelector(".to-text"),
+  exchangeIcon = document.querySelector(".exchange"),
+  selectTag = document.querySelector("select"),
+  icons = document.querySelector(".row i"),
+  translateBtn = document.querySelector("button");
